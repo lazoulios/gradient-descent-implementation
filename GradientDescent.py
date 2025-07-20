e6b9ac6d8181c8603d52c2e94151de273eb46504
lazos <112394398+lazoulios@users.noreply.github.com>
2025-07-20 17:01:00 +0300

    added normalization for x feature
    reaches convergence for whole number cost even faster (14k -> 4k iterations)

--- GradientDescent.py
+++ GradientDescent.py
@@ -4,10 +4,16 @@ import ComputeModel as cm
 import ComputeCost as cc
 import ComputeGradient as cg
 
+#TODO: Add multi linear functionality (more features) 
 x = np.array([12, 25, 30, 42, 73, 15, 24, 26, 53, 72, 84, 16])
 y = np.array([10, 57, 30, 74, 14, 37, 90, 27 ,38 ,49, 42, 69])
 
-m = len(x) #lenght of arrays
+#normalization of the feature
+mu = np.mean(x,axis=0)
+sigma = np.std(x,axis=0)
+x_norm = (x-mu)/sigma
+
+m = len(x_norm) #lenght of arrays
 
 w = 0
 b = 0
@@ -17,17 +23,17 @@ a = float(input('Enter leaning rate of gradient descent (suggested learning rate
 num_iter = int(input('Enter number of iterations for gradient descent: '))
 
 #running gradient descent for the computations of the parameters
-w_final, b_final, cost_history = cg.compute_gradient(x, y, m, w, b, a, num_iter)
+w_final, b_final, cost_history = cg.compute_gradient(x_norm, y, m, w, b, a, num_iter)
 
 #computing the model wiht the final parameters
-f_wb = cm.compute_model(x,w_final,b_final)
+f_wb = cm.compute_model(x_norm,w_final,b_final)
 
 #setup for the two plots
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
 
 #plotting the prediction model
-ax1.plot(x,f_wb,c='b',label='Prediction')
-ax1.scatter(x, y, marker='x', c="red")
+ax1.plot(x_norm,f_wb,c='b',label='Prediction')
+ax1.scatter(x_norm, y, marker='x', c="red")
 ax1.set_xlabel("x")
 ax1.set_title("Testing Gradient Descent")
 ax1.legend()
